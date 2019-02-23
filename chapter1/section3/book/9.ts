6310b8c5f07f7ccd944b2732b901024b66ddf3ee
diaotai <diaotai123@gmail.com>
2019-02-23 18:52:51 +0800

    feat(1-3-book-9): finfish the solution

---
+++ chapter1/section3/book/9.ts
@@ -0,0 +1,39 @@
+/**
+ * 编写一道程序，从标准输入得到一个缺少左括号的表达式并打印出补全括号之后的中序表达式。例如，给定输入:1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) ) 你的程序应该输出((1 + 2) * ((3 - 4) * (5 - 6)))
+ */
+
+function completeString(str: string): string {
+    const valStack: string[] = [];
+    const oprStack: string[] = [];
+    const oprSet: string = '/*+-';
+
+    for (const symbol of str) {
+        if (oprSet.indexOf(symbol) !== -1) {
+            oprStack.push(symbol);
+        } else if (symbol !== ')') {
+            valStack.push(symbol);
+        } else {
+            const operator = oprStack.pop();
+            const val2 = valStack.pop();
+            const val1 = valStack.pop();
+            const temp = `(${val1}${operator}${val2})`;
+            valStack.push(temp);
+        }
+    }
+
+    while (oprStack.length) {
+        const operator = oprStack.pop();
+        const val2 = valStack.pop();
+        const val1 = valStack.pop();
+        const temp = `(${val1}${operator}${val2})`;
+        valStack.push(temp);
+    }
+    return valStack.pop();
+}
+
+function main() {
+    const testStr = '1+2)*3-4)*5-6)))';
+    console.log(completeString(testStr) === '((1+2)*((3-4)*(5-6)))');
+}
+
+main();
