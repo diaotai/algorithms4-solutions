b651825ac08448da8c7953993fc59e875d09804f
diaotai <diaotai123@gmail.com>
2019-02-26 15:27:22 +0800

    feat(chapter2): finish selection sort

---
+++ chapter2/selection.ts
@@ -0,0 +1,22 @@
+/**
+ * 选择排序的实现
+ */
+
+ import { swap, testArraySorted  } from './utils';
+
+function selectionSort(arr: number[]): number[] {
+    for(let i = 0; i < arr.length; i++) {
+        let min = i;
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        swap(arr, i, min);
+    }
+    return arr;
+}
+
+(function () {
+    testArraySorted(selectionSort);
+})()
---
+++ chapter2/utils.ts
@@ -0,0 +1,36 @@
+/**
+ * 一些排序中常用的工具函数
+ */
+
+export function initNumbersWithN(n: number): number[] {
+    const results: number[] = [];
+    for(let i = 0; i < n; i++) {
+        results.push(Math.floor(Math.random() * n));
+    }
+    return results;
+}
+
+export function swap(arr: number[], i: number, j: number): number[] {
+    if (i === j) return arr;
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    return arr;
+}
+
+export function isArraySorted(arr: number[]): boolean {
+    for(let i = 1;i < arr.length; i++) {
+        if (arr[i-1] > arr[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+export function testArraySorted(sort: (arr: number[]) => number[], n = 10) {
+    for (let i = 0; i < n; i++) {
+        const arr = initNumbersWithN(n);
+        sort(arr);
+        console.log(isArraySorted(arr));
+    }
+}
